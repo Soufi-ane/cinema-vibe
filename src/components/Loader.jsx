d2c4c5eb438ff0b3d084fa5e6a55575b658b7faa
soufiane <jabersoufiane605@gmail.com>
2024-02-27 22:37:19 +0100

    center loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { darkModeContext } from "../Context/darkModeContext";
 
-function Loader({ className }) {
+function Loader({ className, isLoading }) {
     const { isDark, secondColor } = useContext(darkModeContext);
     const borderColor = isDark ? "white" : "#535353";
 
     return (
         <div
-            className={`w-full pt-0 md:pt-[40%] lg:pt-[10%] ${
+            className={`w-full ${isLoading ? "pb-72 lg:pb-10" : ""} pt-0 md:pt-[40%] lg:pt-[10%] ${
                 isDark ? secondColor : "bg-white"
             } flex-1 h-screen flex  items-center justify-center ${className}`}>
             <div
