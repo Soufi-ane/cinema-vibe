e7e313c4e677d69382335f5ba18016b4b1dedb9b
soufiane <jabersoufiane605@gmail.com>
2024-02-27 12:49:13 +0100

    change logo to an emoji

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { RiMovie2Line } from "react-icons/ri";
 import { getMovies } from "../omdbApi";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
+import { darkModeContext } from "../Context/darkModeContext";
 
-function Header({ headerType }) {
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+
+function Header() {
     const [searchParams, setSearchParams] = useSearchParams();
     const currentSearch = searchParams.get("s") || "";
     const currentType = searchParams.get("type") || "movie";
@@ -14,6 +17,10 @@ function Header({ headerType }) {
     const [search, setSearch] = useState(currentSearch);
     const [type, setType] = useState(currentType);
     const [year, setYear] = useState(currentYear);
+    const { id } = useParams();
+    const headerType = id ? "show" : "home";
+
+    const { mainColor, secondColor, textColor, isDark, setIsDark } = useContext(darkModeContext);
     const {
         isLoading,
         error,
@@ -39,41 +46,43 @@ function Header({ headerType }) {
 
     return (
         <div
-            className={`flex w-screen fixed justify-between items-center py-3  h-16 ${
-                headerType === "white" ? "bg-white" : "bg-stone-100"
+            className={`flex w-screen   fixed justify-between items-center py-3 z-20  h-16 ${
+                headerType === "show" ? `${mainColor} lg:bg-inherit` : `${mainColor}`
             }`}>
             <div className="hidden lg:flex gap-2 items-center">
-                <RiMovie2Line className="text-purple-600 text-4xl ml-2" />
-                <span className="w-28 font-medium">Cinema Vibe</span>
+                <p className="text-purple-600 text-4xl ml-2">🍿</p>
+                <span
+                    style={{
+                        fontFamily: "Roboto Mono",
+                    }}
+                    className={`${textColor} w-36 text-xl font-medium`}>
+                    Cinema Vibe
+                </span>
             </div>
             <div className="flex gap-2 md:pr-2 lg:pr-20  items-center ">
                 <form
                     onSubmit={HandleSearch}
-                    className="flex items-center px-[10vw] gap-2 sm:w-full w-screen md:gap-5 justify-around">
+                    className="flex items-center px-[10vw] gap-2 sm:w-full w-screen md:gap-3 justify-around">
                     <input
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                         maxLength={20}
-                        className="py-1 rounded-full md:mr-5 px-3 md:w-72 w-[50vw] sm:w-[35vw] md:focus:w-80 focus:outline-8 outline-stone-300 text-sm md:focus:py-1.5 transition-all border-2 border-stone-300 sm:-order-1 order-2"
+                        className={`py-1 rounded-full md:mr-5 px-3 md:w-64 w-[50vw] sm:w-[35vw] md:focus:w-80 focus:outline-8 outline-stone-300 text-sm md:focus:py-1.5 transition-all border-2 border-stone-300 sm:-order-1 order-2 ${secondColor} ${textColor}`}
                         type="text"
                         placeholder="Search"
                     />
                     <button
-                        className={`py-1 hidden sm:inline px-6 ${
-                            headerType === "white" ? "bg-stone-100" : "bg-white"
-                        } rounded-full `}>
+                        className={`py-1 hidden sm:inline ${textColor} ${secondColor} px-6 rounded-full `}>
                         Search
                     </button>
                     <button
                         className={`sm:hidden order-last p-2 ${
-                            headerType === "white" ? "bg-stone-100" : "bg-white"
+                            headerType === "show" ? "bg-stone-100" : "bg-white"
                         } rounded-full`}>
                         <IoSearch />
                     </button>
                     <select
-                        className={`${
-                            headerType === "white" ? "bg-stone-100" : "bg-white"
-                        } rounded-full py-1 px-3 text-sm focus:outline-8 outline-stone-300 sm:order-first lg:order-1`}
+                        className={`rounded-full py-1 px-3 text-sm focus:outline-8 outline-stone-300 sm:order-first ${textColor} ${secondColor}  lg:order-1`}
                         name="type"
                         value={type}
                         onChange={(e) => setType(e.target.value)}>
@@ -81,7 +90,7 @@ function Header({ headerType }) {
                         <option value="series">Series</option>
                     </select>
                     <input
-                        className="py-1 hidden sm:inline  rounded-full mr-10 px-4 w-28 focus:outline-8 outline-stone-300 text-sm  transition-all border-2 border-stone-300 lg:order-last"
+                        className={`py-1 hidden sm:inline  rounded-full px-4 w-28 focus:outline-8 outline-stone-300 text-sm  transition-all border-2 border-stone-300 ${textColor} ${secondColor} lg:order-last`}
                         type="number"
                         placeholder="year"
                         value={year}
@@ -89,6 +98,17 @@ function Header({ headerType }) {
                             setYear(() => (e.target.value > thisYear ? thisYear : e.target.value));
                         }}
                     />
+
+                    <button
+                        onClick={() => setIsDark((curr) => !curr)}
+                        className={`hidden md:inline lg:order-last rounded-full text-sm p-2 ${secondColor} `}
+                        type="reset">
+                        {!isDark ? (
+                            <MdDarkMode className="text-lg" />
+                        ) : (
+                            <MdLightMode className="text-yellow-600 text-lg" />
+                        )}
+                    </button>
                 </form>
             </div>
         </div>
